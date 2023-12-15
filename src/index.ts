3426848813068bf779acb58f9d29230585392923
Byurhan Nurula <imbyurhan@gmail.com>
2023-12-15 20:37:45 +0200

    Update canister - add init canister method, add check to for authorized principle & small updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {
   Canister,
+  Err,
   ic,
   nat64,
   Ok,
@@ -10,121 +11,140 @@ import {
   StableBTreeMap,
   text,
   update,
+  Variant,
   Vec,
-  Void,
 } from "azle";
 
 import { POSSIBLE_ACTIONS } from "./constants";
+import { generateId } from "./utils";
 
 const User = Record({
   id: Principal,
-  name: text,
   createdAt: nat64,
 });
 
 const LogRecord = Record({
   id: Principal,
   user: Principal,
-
   action: text,
-  dataName: text,
-  organizationId: text,
-
+  metadata: text,
   createdAt: nat64,
+});
+
+const Error = Variant({
+  NotFound: text,
+  Conflict: text,
+  Unauthorized: text,
+  InvalidPayload: text,
 });
 
 const userStorage = StableBTreeMap(Principal, User, 0);
 const logStorage = StableBTreeMap(Principal, LogRecord, 1);
 
 export default Canister({
-  addAuthorizedUser: update([Principal], Void, (newPrincipal) => {
-    // Check if the caller is an authorized principal
-    // If so, add newPrincipal to the list of authorized principals
-    // Otherwise, reject the call
+  /**
+   * Initializes the canister by adding a new user during deployment.
+   * @param userId - The Principal ID of the new user.
+   */
+  initializeCanister: update([Principal], Result(User, Error), (userId) => {
+    if (!ic.caller().compareTo(ic.id())) {
+      return Err({ Unauthorized: "Unauthorized access!" });
+    }
+
+    if (userStorage.containsKey(userId)) {
+      return Err({ Conflict: "User already exists!" });
+    }
+
+    const newUser: typeof User = {
+      id: userId,
+      createdAt: ic.time(),
+    };
+
+    userStorage.insert(userId, newUser);
+
+    return Ok(newUser);
   }),
 
-  addLog: update(
-    [text, text, text],
-    Result(LogRecord, text),
-    (action, dataName, organizationId) => {
-      if (!isUserHasAccess()) {
-        throw new Error("You do not have access!");
-      }
-      if (!POSSIBLE_ACTIONS.includes(action.toLowerCase())) {
-        throw new Error(
-          `'${action}' is not supported, please select one of: ${POSSIBLE_ACTIONS}`,
-        );
-      }
+  /**
+   * Adds a log entry with the provided details.
+   * @param action - The action performed.
+   * @param dataName - The name of the data.
+   * @param organizationId - The ID of the organization.
+   * @returns The log entry.
+   */
+  addLog: update([text, text], Result(LogRecord, Error), (action, metadata) => {
+    if (!userStorage.containsKey(ic.caller())) {
+      return Err({ Unauthorized: "Unauthorized access!" });
+    }
 
-      const id = generateId();
-      const log: typeof LogRecord = {
-        id,
-        user: ic.caller(),
+    // Check if the action is supported
+    if (!POSSIBLE_ACTIONS.includes(action.toLowerCase())) {
+      return Err({
+        InvalidPayload: `'${action}' is not supported, please select one of: ${POSSIBLE_ACTIONS}`,
+      });
+    }
 
-        dataName,
-        organizationId,
-        action: action.toLowerCase(),
+    const log: typeof LogRecord = {
+      id: generateId(),
+      user: ic.caller(),
+      metadata,
+      action: action.toLowerCase(),
+      createdAt: ic.time(),
+    };
 
-        createdAt: ic.time(),
-      };
+    logStorage.insert(log.id, log);
 
-      logStorage.insert(log.id, log);
+    return Ok(log);
+  }),
 
-      return Ok(log);
-    },
-  ),
-
-  getLogs: query([], Result(Vec(LogRecord), text), () => {
-    if (!isUserHasAccess()) {
-      throw new Error("You do not have access!");
+  /**
+   * Retrieves all logs stored in the canister.
+   * @returns A list of log entries.
+   */
+  getLogs: query([], Result(Vec(LogRecord), Error), () => {
+    if (!userStorage.containsKey(ic.caller())) {
+      return Err({ Unauthorized: "Unauthorized access!" });
     }
 
     return Ok(logStorage.values());
   }),
 
-  getLogsByAction: query([text], Result(Vec(LogRecord), text), (action) => {
-    if (!isUserHasAccess()) {
-      throw new Error("You do not have access!");
-    }
-    if (!POSSIBLE_ACTIONS.includes(action.toLowerCase())) {
-      throw new Error(
-        `'${action}' is not supported, please select one of: ${POSSIBLE_ACTIONS}`,
-      );
+  /**
+   * Retrieves logs filtered by a specific action.
+   * @param action - The action to filter logs by.
+   * @returns A list of log entries matching the specified action.
+   */
+  getLogsByAction: query([text], Result(Vec(LogRecord), Error), (action) => {
+    if (!userStorage.containsKey(ic.caller())) {
+      return Err({ Unauthorized: "Unauthorized access!" });
     }
 
-    return Ok(
-      logStorage
-        .values()
-        .filter(
-          (x: typeof LogRecord) =>
-            x.action.toLowerCase() === action.toLowerCase(),
-        ),
-    );
+    // Check if the provided action is supported
+    if (!POSSIBLE_ACTIONS.includes(action.toLowerCase())) {
+      return Err({
+        InvalidPayload: `'${action}' is not supported, please select one of: ${POSSIBLE_ACTIONS}`,
+      });
+    }
+
+    const filteredLogs = logStorage
+      .values()
+      .filter(
+        (x: typeof LogRecord) =>
+          x.action.toLowerCase() === action.toLowerCase(),
+      );
+
+    return Ok(filteredLogs);
   }),
 
-  getUsersLogs: query([Principal], Result(Vec(LogRecord), text), (id) => {
-    if (!isUserHasAccess()) {
-      throw new Error("You do not have access!");
+  /**
+   * Generates an ID of type Principal using UUID.
+   * @returns a Principal ID.
+   */
+  generateId: query([], Result(Principal, Error), () => {
+    if (!userStorage.containsKey(ic.caller())) {
+      return Err({ Unauthorized: "Unauthorized access!" });
     }
 
-    return Ok(
-      logStorage.values().filter((x: typeof LogRecord) => x.user === id),
-    );
+    return Ok(generateId());
   }),
 });
-
-/**
- * Generate an ID of a type Principal.
- * @returns a Principal ID.
- */
-function generateId(): Principal {
-  const randomBytes = new Array(29)
-    .fill(0)
-    .map(() => Math.floor(Math.random() * 256));
-
-  return Principal.fromUint8Array(Uint8Array.from(randomBytes));
-}
-
-function isUserHasAccess(): boolean {
-  return userStorage.values().find((x: typeof User) => x.id === ic.caller());
-}
